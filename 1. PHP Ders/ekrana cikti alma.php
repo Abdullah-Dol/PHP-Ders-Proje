522130f885091b8b57d34a748f574a5365765cb0
Abdullah DOL <72069928+Abdullah-Dol@users.noreply.github.com>
2022-03-12 19:01:37 +0300

    1

---
+++ 1. PHP Ders/ekrana cikti alma.php
@@ -0,0 +1,24 @@
+// ekrana cikti alma cesitleri:
+
+<?php 
+
+echo "php ilk ders";
+
+?>
+----------
+<?php 
+
+print("php ilk dersim.");
+
+?>
+-------------
+<?="php ile ilk ders";
+
+?>
+
+<?php 
+
+echo "php ilk ders <br>"; // br komutu yazıyı alt alta yazmamızı saglar.
+echo "devam ediyor!";
+
+?>
